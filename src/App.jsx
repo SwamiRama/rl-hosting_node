f3b942a7e9d3445d2f99cd921fc465e1b54df5a7
Jimmi <git@rl-hosting.de>
2017-06-04 18:28:25 +0200

    refactoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 import './App.css';
 
 import Navbar from './components/NavBar/index.jsx';
+
 import TicTacToe from './scenes/TicTacToe/components/Game/index.jsx';
 import Home from './scenes/Home/index.js'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar />
-        <Routing />
-      </div>
+      <Router>
+        <div>
+          <Navbar/>
+          <Routing/>
+        </div>
+      </Router>
     );
   }
 }
 
 const Routing = () => (
   <main>
-    <Switch>
-      <Route exact path='/' component={Home}/>
-      <Route path='/games/tictactoe' component={TicTacToe}/>
-    </Switch>
+    <Route exact path='/' component={Home}/>
+    <Route path='/games/tictactoe' component={TicTacToe}/>
   </main>
 )
 
