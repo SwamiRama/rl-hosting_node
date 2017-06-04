ca37e8fe7a49d7fa90c6b1fe30df248d9792cafc
Jimmi <git@rl-hosting.de>
2017-06-04 18:28:55 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,18 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
-import registerServiceWorker from './registerServiceWorker';
+
 import './index.css';
 
-import { BrowserRouter, Route } from 'react-router-dom'
-
-//ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
-
-ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
-    document.getElementById('root')
-);
+ReactDOM.render( < App / > , document.getElementById('root'));
